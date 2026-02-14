b0abea3028a8ba4aa2e31f1fd6aeb3a618b5ff1e
Sumit-Saurabh98 <sumitsaurabh112@gmail.com>
2026-02-15 00:14:22 +0530

    tools and agent

--- notebooks/08_tools.py
+++ notebooks/08_tools.py
@@ -29,7 +29,20 @@ def multiplyNumber(num1:int, num2:int) -> int:
         Return:
          int: multiplication of two numbers
     """
-    return num1 * num2    
+    return num1 * num2  
+
+
+@tool("web_search")
+def  search(query:str) -> str:
+    """
+    Search the web for information. Particularly the crickbuzz.com.
+
+    Args(str): user query
+
+    Return(str): search result
+    """
+
+    return f"Result for: {query}"
 
 
 # res = addNumber.invoke({"num1":10, "num2":20})
@@ -38,9 +51,11 @@ def multiplyNumber(num1:int, num2:int) -> int:
 
 llm = ChatOpenAI(model='gpt-4')
 
-agents = create_agent(model=llm, tools=[addNumber], system_prompt="You are a math teacher and always use tools for calculation.")
+# agents = create_agent(model=llm, tools=[addNumber, multiplyNumber, search], system_prompt="You are a math teacher and always use tools for calculation.")
 
-response = agents.invoke({"messages":[{"role":"user", "content":"What is 10 + 20"}]})
+agents = create_agent(model=llm, tools=[search], system_prompt="You are a cricket experts and you provide the answer for all the question based on you knowledge.")
+
+response = agents.invoke({"messages":[{"role":"user", "content":"Tomorrow India ans Pakistan has world cup 2026 match in Sri Lanka. What will be the probable 11 for both the team?"}]})
 
 # for res in response['messages']:
 #     print(res)
