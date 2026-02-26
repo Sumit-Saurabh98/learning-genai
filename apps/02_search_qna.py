626395ffac52a147c8109a9fb3d32eefe89f3267
Sumit-Saurabh98 <sumitsaurabh112@gmail.com>
2026-02-27 00:55:12 +0530

    Added memory to agent

--- apps/02_search_qna.py
+++ apps/02_search_qna.py
@@ -5,6 +5,7 @@ from langchain_community.utilities import GoogleSerperAPIWrapper
 from langchain_groq import ChatGroq
 from langchain.agents import create_agent
 from langchain_core.tools import Tool, tool
+from langgraph.checkpoint.memory import InMemorySaver 
 
 
 llm = ChatGroq(model="llama-3.3-70b-versatile", temperature=0)
@@ -17,12 +18,16 @@ def google_search(query: str) -> str:
 agent = create_agent(
     model=llm,
     tools=[google_search],
-    system_prompt="You are a helpful assistant and you have access to google search tool. Use it when needed."
+    system_prompt="You are a helpful assistant and you have access to google search tool. Use it when needed.",
+    checkpointer=InMemorySaver()
 )
 
 while True:
     question = input("User: ")
     if question.lower() == "exit":
         break
-    response = agent.invoke({"messages":[{"role":"user","content":question}]})
+    response = agent.invoke(
+        {"messages": [{"role": "user", "content": question}]},
+        config={"configurable": {"thread_id": "1"}}
+    )
     print("AI: ", response["messages"][-1].content)
